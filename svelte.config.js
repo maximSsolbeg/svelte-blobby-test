0a1315322af0e71c75688115d75ae8dc7c1253ff
some developer <web375@gmail.com>
2024-08-17 01:17:56 +0300

    Configured static adapter and disabled strict mode for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,15 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			strict: false,
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/svelte-blobby-test' : ''
+		}
 	},
 
 	vite: {
